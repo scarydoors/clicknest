806032dbb433ed4b846afd245a911abdf71c6846
Alan Strauhs <scarydoorsyeah@gmail.com>
2025-07-13 15:53:48 +0100

    feat: ping pong

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func getPing(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "pong")
+}
+
 func main() {
-	println("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /ping", getPing)
+
+	fmt.Printf("Serving server at port :6969\n")
+	if err := http.ListenAndServe(":6969", mux); errors.Is(err, http.ErrServerClosed){
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s", err)
+		os.Exit(1)
+	}
 }
