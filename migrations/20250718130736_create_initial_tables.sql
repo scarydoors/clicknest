cc236a46e227bbf4d796c54401eb786c7bf92501
Alan Strauhs <scarydoorsyeah@gmail.com>
2025-07-28 00:08:24 +0100

    wip: sessions table

---
+++ migrations/20250718130736_create_initial_tables.sql
@@ -0,0 +1,34 @@
+-- +goose Up
+CREATE TABLE events
+(
+    timestamp DateTime CODEC(Delta(4), ZSTD),
+    domain LowCardinality(String),
+    kind LowCardinality(String),
+    session_id UInt64,
+    user_id UInt64,
+    pathname String,
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(timestamp)
+PRIMARY KEY (domain, toDate(timestamp), kind, user_id)
+ORDER BY (domain, toDate(timestamp), kind, user_id, timestamp);
+
+CREATE TABLE sessions
+(
+    start DateTime CODEC(Delta(4), ZSTD),
+    end DateTime CODEC(Delta(4), ZSTD),
+    domain LowCardinality(String),
+    duration UInt32,
+    event_count UInt32,
+    session_id UInt64,
+    user_id UInt64,
+    sign Int8,
+)
+ENGINE = VersionedCollapsingMergeTree(sign, event_count)
+PARTITION BY toYYYYMM(start)
+PRIMARY KEY (domain, toDate(start), user_id, session_id)
+ORDER BY (domain, toDate(start), user_id, session_id, start);
+
+-- +goose Down
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS sessions;
