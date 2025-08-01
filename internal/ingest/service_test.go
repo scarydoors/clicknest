79fddcde9ca0d46ff4c42564c2bb150023269545
Alan Strauhs <alan.strauhs@pagabogroup.com>
2025-08-01 16:22:28 +0100

    wip: testing cancellation and flushes

--- internal/ingest/service_test.go
+++ internal/ingest/service_test.go
@@ -2,49 +2,50 @@ package ingest_test
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"log/slog"
 	"testing"
 	"time"
 
 	"github.com/scarydoors/clicknest/internal/analytics"
 	"github.com/scarydoors/clicknest/internal/ingest"
-	"github.com/stretchr/testify/assert"
+	//"github.com/stretchr/testify/assert"
 )
 
 type mockStorage struct {}
 
-var processedItemsNormally bool
-var finalFlushExecuted bool
-var batchInsertTimesCalled int
-
-func (s *mockStorage) BatchInsertEvent(ctx context.Context, _ []analytics.Event) error {
-	batchInsertTimesCalled++
-	_, ok := ctx.Deadline()
-	if ok {
-		finalFlushExecuted = true
+func (s *mockStorage) BatchInsertEvent(ctx context.Context, events []analytics.Event) error {
+	log.Default().Print("processing", events, "\n")
+	if _, ok := ctx.Deadline(); ok {
+		log.Default().Print("cleaning", "\n")
+	} else if len(events) == 1 {
+		log.Default().Print("handleing 1 event case")
+		select {
+		case <-time.After(60 * time.Second):
+		case <-ctx.Done():
+			return ctx.Err()
+		}
 	} else {
-		processedItemsNormally = true
+		log.Default().Print("normal run")
 	}
-
-	return nil
+	log.Default().Print("____ ")
+	return nil;
 }
 
 func TestServiceWorkersLifecycle(t *testing.T) {
 	service := ingest.NewService(&mockStorage{}, slog.Default())
 	service.StartWorkers(ingest.WorkerConfig{
-		FlushInterval: 60 * time.Second,
+		FlushInterval: 2 * time.Second,
 		FlushLimit: 2,
 	})
 
-	for range 3 {
-		service.IngestEvent(context.Background(), analytics.Event{})
+	for n := range 3 {
+		service.IngestEvent(context.Background(), analytics.Event{Domain: fmt.Sprintf("%d", n)})
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 60 * time.Second)
 	defer cancel()
+	time.Sleep(5 * time.Second)
 	service.ShutdownWorkers(ctx)
-
-	assert.Equal(t, 2, batchInsertTimesCalled)
-	assert.True(t, processedItemsNormally)
-	assert.True(t, finalFlushExecuted)
 }
