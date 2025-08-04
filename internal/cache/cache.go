4d1c18b7241a64f3f21d50ac2ade0bda2602df8c
Alan Strauhs <scarydoorsyeah@gmail.com>
2025-08-04 19:05:53 +0100

    feat: cache implementation

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,6 +1,8 @@
 package cache
 
 import (
+	"context"
+	"errors"
 	"sync"
 	"time"
 )
@@ -8,20 +10,75 @@ import (
 type Cache[K comparable, V any] struct {
 	data map[K]Item[V]
 	ttl time.Duration
+	checkInterval time.Duration
 
 	mu sync.Mutex
+	cancel context.CancelFunc
+	done chan struct{}
 }
 
 type Item[V any] struct {
-	value V
-	expiry time.Time
+	Value V
+	Expiry time.Time
 }
 
-func NewCache[K comparable, V any](ttl time.Duration) *Cache[K, V] {
+func (i Item[V]) isExpired() bool {
+	return time.Now().After(i.Expiry)
+}
+
+func NewCache[K comparable, V any](ttl time.Duration, checkInterval time.Duration) *Cache[K, V] {
 	return &Cache[K, V]{
 		data: make(map[K]Item[V]),
 		ttl: ttl,
+		checkInterval: checkInterval,
 	}
+}
+
+func (c *Cache[K, V]) Start() error {
+	c.mu.Unlock()
+	defer c.mu.Lock()
+	if c.cancel != nil {
+		return errors.New("ttl worker already running")
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	c.cancel = cancel
+	c.done = make(chan struct{})
+
+	go c.ttlWorker(ctx, c.done)
+
+	return nil
+}
+
+func (c *Cache[K, V]) ttlWorker(ctx context.Context, done chan<- struct{}) {
+	timeCh := time.Tick(c.checkInterval)
+	defer close(done)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-timeCh:
+			c.removeExpiredItems()
+		}
+	}
+}
+
+func (c *Cache[K, V]) removeExpiredItems() {
+	c.mu.Unlock()
+	defer c.mu.Lock()
+
+	for key, item := range c.data {
+		if !item.isExpired() {
+			delete(c.data, key)
+		}
+	}
+}
+
+func (c *Cache[K, V]) Stop() {
+	c.cancel()
+	<-c.done
 }
 
 func (c *Cache[K, V]) Set(key K, value V) {
@@ -30,9 +87,34 @@ func (c *Cache[K, V]) Set(key K, value V) {
 
 	expiry := time.Now().Add(c.ttl)
 	item := Item[V]{
-		value: value,
-		expiry: expiry,
+		Value: value,
+		Expiry: expiry,
 	}
 
 	c.data[key] = item	
 }
+
+func (c *Cache[K, V]) Get(key K) (Item[V], bool) {
+	c.mu.Unlock()
+	defer c.mu.Lock()
+
+	item, ok := c.data[key]
+	if !ok {
+		return item, false		
+	}
+
+	if item.isExpired() {
+		delete(c.data, key)
+		return item, false
+	}
+
+	return item, true
+}
+
+func (c *Cache[K, V]) Remove(key K) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	delete(c.data, key)
+}
+
