4c2be1849d23ebe0bdeda0a84bee7497baa5dfbc
Alan Strauhs <scarydoorsyeah@gmail.com>
2025-08-09 00:55:12 +0100

    wip

--- internal/analytics/session.go
+++ internal/analytics/session.go
@@ -18,25 +18,30 @@ func NewSessionID() SessionID {
 }
 
 type Session struct {
-	Start      time.Time
-	End        time.Time
-	Domain     string
-	Duration   uint32
-	EventCount uint32
-	SessionID  SessionID
-	UserID     UserID
-	Sign       int8
+	start      time.Time
+	end        time.Time
+	domain     string
+	duration   uint32
+	eventCount uint32
+	sessionID  SessionID
+	userID     UserID
+	sign       int8
 }
 
 func FromEvent(event Event) Session {
 	return Session{
-		Start:      event.Timestamp,
-		End:        event.Timestamp,
-		Domain:     event.Domain,
-		Duration:   0,
-		EventCount: 1,
-		SessionID:  NewSessionID(),
-		UserID:     event.UserID,
-		Sign:       1,
+		start:      event.Timestamp,
+		end:        event.Timestamp,
+		domain:     event.Domain,
+		duration:   0,
+		eventCount: 1,
+		sessionID:  NewSessionID(),
+		userID:     event.UserID,
+		sign:       1,
 	}
+}
+
+func (s *Session) Update(event Event) {
+	s.end = event.Timestamp
+	s.duration = uint32(s.end.Sub(s.start).Milliseconds()) 
 }
--- migrations/20250718130736_create_initial_tables.sql
+++ migrations/20250718130736_create_initial_tables.sql
@@ -19,13 +19,12 @@ CREATE TABLE sessions
     end DateTime CODEC(Delta(4), ZSTD),
     domain LowCardinality(String),
     duration UInt32,
-    event_count UInt32,
     session_id UInt64,
     user_id UInt64,
     sign Int8,
     INDEX minmax_end end TYPE minmax GRANULARITY 1,
 )
-ENGINE = VersionedCollapsingMergeTree(sign, event_count)
+ENGINE = VersionedCollapsingMergeTree(sign, end)
 PARTITION BY toYYYYMM(start)
 PRIMARY KEY (domain, toDate(start), user_id, session_id)
 ORDER BY (domain, toDate(start), user_id, session_id, start);
