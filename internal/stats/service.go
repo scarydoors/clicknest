a3acc04a726ded089fa007c3aa3f62723ba7898c
Alan Strauhs <alan.strauhs@pagabogroup.com>
2025-08-28 20:48:12 +0100

    wip: scaffolding

---
+++ internal/stats/service.go
@@ -0,0 +1,24 @@
+package stats
+
+import (
+	"log/slog"
+	"time"
+)
+
+type Service struct {
+	logger *slog.Logger
+}
+
+func NewService(logger *slog.Logger) Service {
+	return Service{
+		logger: logger,
+	}
+}
+
+type PageviewEntry struct {
+	time time.Time
+	value uint64
+}
+
+type PageviewResult []PageviewEntry
+func (s *Service) GetPageviews() 
