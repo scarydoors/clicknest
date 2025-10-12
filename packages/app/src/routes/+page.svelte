7f37aa0544eccc0572a0862ff8912ead571ed9bd
Alan Strauhs <scarydoorsyeah@gmail.com>
2025-10-12 17:40:35 +0100

    feat: data fetching timeseries to display on graph

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -3,27 +3,33 @@ import * as Card from "$lib/components/ui/card/index"
 import * as Chart from "$lib/components/ui/chart/index";
 import { AreaChart } from "layerchart";
 import { scaleUtc } from "d3-scale";
-	import { curveLinear } from "d3-shape";
-
-const chartData = [
-    { date: new Date("2024-01-01"), value: 186 },
-    { date: new Date("2024-02-01"), value: 305 },
-    { date: new Date("2024-03-01"), value: 237 },
-    { date: new Date("2024-04-01"), value: 73 },
-    { date: new Date("2024-05-01"), value: 209 },
-    { date: new Date("2024-06-01"), value: 214 },
-];
+import { curveLinear } from "d3-shape";
+import { onMount } from "svelte";
 
 const chartConfig = {
     value: {label:"Pageviews", color: "var(--chart-1)"}
 } satisfies Chart.ChartConfig;
+
+type Timeseries = { timestamp: Date, value: number }[]
+
+let loading = $state<boolean>(true);
+let data = $state<Timeseries | undefined>();
+
+onMount(() => {
+    fetch("http://localhost:6969/api/timeseries").then(async (resp) => {
+        const json = await resp.json();
+        data = json.map((t: {timestamp: string, value: number}) => ({ ...t, timestamp: new Date(t.timestamp) }))
+        loading = false;
+    })
+})
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
 <Chart.Container config={chartConfig}>
     <AreaChart
-        x="date"
+        x="timestamp"
         series={[
             {
                 key: "value",
@@ -31,7 +37,7 @@ const chartConfig = {
                 color: chartConfig.value.color
             }
         ]}
-        data={chartData}
+        data={data}
         xScale={scaleUtc()}
         props={{
             area: {
