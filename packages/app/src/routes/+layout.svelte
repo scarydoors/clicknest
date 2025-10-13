da9caae57ad4c4057b1b94224ebacf22f6379e06
Alan Strauhs <scarydoorsyeah@gmail.com>
2025-10-13 20:36:33 +0100

    feat: sidebar

--- packages/app/src/routes/+layout.svelte
+++ packages/app/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+    import * as Sidebar from '$lib/components/ui/sidebar/index.ts';
+    import AppSidebar from '$lib/components/app-sidebar.svelte';
 
 	let { children } = $props();
 </script>
@@ -9,4 +11,10 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<Sidebar.Provider>
+    <AppSidebar />
+    <main>
+        <Sidebar.Trigger />
+        {@render children?.()}
+    </main>
+</Sidebar.Provider>
--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -27,34 +27,35 @@ onMount(() => {
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<Chart.Container config={chartConfig}>
-    <AreaChart
-        x="timestamp"
-        series={[
-            {
-                key: "value",
-                label: "Pageviews",
-                color: chartConfig.value.color
-            }
-        ]}
-        data={data}
-        xScale={scaleUtc()}
-        props={{
-            area: {
-                curve: curveLinear,
-                "fill-opacity": 0.4,
-                line: { class: "stroke-1" },
-                motion: "tween",
-            },
-            xAxis: {
-                format: (v: Date) => v.toLocaleDateString(undefined, { month: "short", day: "numeric" }),
-                ticks: data?.length ?? 0
-            },
-            yAxis: { format: () => "" },
-        }}
-    >
-        {#snippet tooltip()}
-            <Chart.Tooltip hideLabel />
-        {/snippet}
-    </AreaChart>
-</Chart.Container>
+<Card.Root>
+    <Chart.Container config={chartConfig}>
+        <AreaChart
+            x="timestamp"
+            series={[
+                {
+                    key: "value",
+                    label: "Pageviews",
+                    color: chartConfig.value.color
+                }
+            ]}
+            data={data}
+            xScale={scaleUtc()}
+            props={{
+                area: {
+                    curve: curveLinear,
+                    "fill-opacity": 0.4,
+                    line: { class: "stroke-1" },
+                },
+                xAxis: {
+                    format: (v: Date) => v.toLocaleDateString(undefined, { month: "short", day: "numeric" }),
+                    ticks: data?.length ?? 0
+                },
+                yAxis: { format: () => "" },
+            }}
+        >
+            {#snippet tooltip()}
+                <Chart.Tooltip hideLabel />
+            {/snippet}
+        </AreaChart>
+    </Chart.Container>
+</Card.Root>
