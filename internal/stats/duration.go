3f7b4ca287b5b9473bec4bb04b8f0f8dc47671fe
Alan Strauhs <strauhsalan@gmail.com>
2025-12-08 23:13:35 +0000

    wip: add empty function for accurate duration

---
+++ internal/stats/duration.go
@@ -0,0 +1,8 @@
+package stats
+
+import "time"
+
+func ParseDurationWithDate(duration string, startDate time.Time) time.Duration {
+	// TODO: support duration with d, m, and y components
+	return time.Duration(0)
+}
