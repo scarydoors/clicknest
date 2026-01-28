be2a522e8202015d5a2a0c4057f6904373a5b69f
Alan Strauhs <strauhsalan@gmail.com>
2026-01-28 23:15:21 +0000

    wip: create register flow testing

---
+++ packages/app/src/routes/(identity)/register/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { PUBLIC_KRATOS_API_URL } from "$env/static/public"
+import { Configuration, FrontendApi, UpdateRegistrationFlowBodyFromJSON, type RegistrationFlow } from "@ory/client-fetch";
+import { onMount } from "svelte";
+
+const frontendClient = new FrontendApi(
+    new Configuration({
+        headers: {
+            Accept: "application/json",
+        },
+        basePath: PUBLIC_KRATOS_API_URL
+    })
+)
+
+async function createRegisterFlow() {
+    const registerFlow = await frontendClient.createBrowserRegistrationFlow();
+
+    return registerFlow
+}
+
+
+// TODO: create 
+onMount(() => {
+    createRegisterFlow().then((flow) => {
+        console.log(flow);
+    })
+})
+</script>
+
